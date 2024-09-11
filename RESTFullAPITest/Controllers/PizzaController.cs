6fb09e1b8e14dfb0585d7a299711509494851d43
Ilia Baev <baevilia.201@yandex.ru>
2024-09-11 14:30:49 +0300

    Добавлен контроллер с методами GetAll и Get

---
+++ RESTFullAPITest/Controllers/PizzaController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RESTFullAPITest.Models;
+using RESTFullAPITest.Services;
+
+namespace RESTFullAPITest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        public PizzaController()
+        {
+            
+        }
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() =>
+            PizzaService.GetAll();
+
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id) 
+        {
+            var pizza = PizzaService.Get(id);
+
+            if(pizza ==null)
+                return NotFound();
+
+            return pizza;
+        }
+    }
+}
